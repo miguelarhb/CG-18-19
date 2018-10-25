8c5a647004a4858da9563118bd3a8281fa04c593
quickacid <38334667+quickacid@users.noreply.github.com>
2018-10-25 15:09:30 +0100

    final
    -tudo working
    -menos ,passdo uma velocidade aquilo tripasse um bocado

--- trab2/js/jogo.js
+++ trab2/js/jogo.js
@@ -8,9 +8,11 @@ var wHeight = window.innerHeight;
 var originalAspect = wWidth / wHeight;
 var floor,parede1,parede2,parede3,parede4,num;
 var ball=new Array();
-var accel_max=1.5;
-var ball_num=3;
-
+var accel_max=1;
+var ball_num=10;
+var perspetiva,ortegonal,ball_camera;
+var ZOOM=50;
+var ASPECT=1;
 
 class Entity extends THREE.Object3D{
     constructor(x,y,z){
@@ -34,7 +36,7 @@ class Ball extends Entity{
 
         //this.teta=310;
         this.velocity=0;
-        console.log(this.acceler.x, this.acceler.z);
+        //console.log(this.acceler.x, this.acceler.z);
         geometry=new THREE.SphereGeometry(Math.sqrt(250*250+125*125)/20,32,32);
         material=new THREE.MeshBasicMaterial({color:0xff4000,wireframe:false});
         var helper=new THREE.AxisHelper(20);
@@ -45,6 +47,7 @@ class Ball extends Entity{
         this.position.set(this.pos_x,y+(Math.sqrt(250*250+125*125)/20),this.pos_z);
 
         scene.add(this);
+
     }
     put_x_z(x,z){
         this.pos_z=z;
@@ -70,6 +73,13 @@ class Ball extends Entity{
     change_position(){
 
         this.position.set(this.pos_x,(Math.sqrt(250*250+125*125)/20),this.pos_z);
+        if(ball_camera==1){
+            
+            camera.position.z=ball[0].get_pos_z()+ (ball[0].get_acceler_z() / (Math.abs(ball[0].get_acceler_z()))*ball[0].get_radius()*3)  ;
+            camera.position.x=ball[0].get_pos_x()+ (ball[0].get_acceler_x() / (Math.abs(ball[0].get_acceler_x())) *ball[0].get_radius()*3);
+            camera.position.y=40;
+            camera.lookAt(ball[0].position);
+        }
     }
     change_velocity(a){
         this.velocity=a;
@@ -139,6 +149,25 @@ class Wall extends Entity{
     }
 }
 
+function add_accel(){
+    var i=0;
+    console.log('aumentei');
+    for(i=0;i<ball_num;i++){
+        if(ball[i].get_acceler_x()<0){
+            ball[i].change_accel_x_to(ball[i].get_acceler_x()-0.2);
+        }
+        if(ball[i].get_acceler_z()<0){
+            ball[i].change_accel_z_to(ball[i].get_acceler_z()-0.2);
+        }
+        if(ball[i].get_acceler_x()>0){
+            ball[i].change_accel_x_to(ball[i].get_acceler_x()+0.2);
+        }
+        if(ball[i].get_acceler_z()>0){
+            ball[i].change_accel_z_to(ball[i].get_acceler_z()+0.2);
+        }
+    }
+}
+
 function create_walls(){
     parede1=new Wall(0,0,-63.5,250,0);
     parede2=new Wall(0,0,63.5,250,0);
@@ -149,8 +178,9 @@ function check_wall(b){
     var prox_x=0;
     var prox_z=0;
     var s=0;
-    prox_x=b.get_pos_x();
-    prox_z=b.get_pos_z();
+    prox_x=b.get_pos_x()+b.get_acceler_x();
+    prox_z=b.get_pos_z()+b.get_acceler_z();
+    //console.log(prox_x);
     if(prox_x<0)
         prox_x=-prox_x;
     if(prox_z<0)
@@ -166,8 +196,8 @@ function check_wall(b){
         b.change_accel_z();
     }    
     if(s>0){
-        var position_z= b.get_pos_z() +parseFloat(b.get_acceler_z());
-        var position_x= b.get_pos_x() + parseFloat(b.get_acceler_x());
+        var position_z= b.get_pos_z() +parseFloat(b.get_acceler_z())/2;
+        var position_x= b.get_pos_x() + parseFloat(b.get_acceler_x())/2;
         b.put_x_z(position_x,position_z);
         b.change_position();       
     }
@@ -175,19 +205,13 @@ function check_wall(b){
 }
 function collision(x,z,k){
     var a=0;
-    var b=0;
     var j=0;
     for(j=0;j<ball_num;j++){
-        var new_x=ball[j].pos_x -x;
-        var new_z=ball[j].pos_z -z;
-        var distancia=Math.sqrt((new_x*new_x)+(new_z*new_z))
+
         if(j!=k){
             if(checkCollision(ball[k],ball[j]))
                 a++;
         }
-        
-
-        //check_wall(ball[k])
     }
 
     
@@ -207,8 +231,11 @@ function rotate(x, z, sin, cos, reverse) {
 }
 
 function checkCollision (ball0, ball1) {
-  var dx = ball1.get_pos_x() - ball0.get_pos_x();
-  var dz = ball1.get_pos_z() - ball0.get_pos_z();
+  var posit_z= ball0.get_pos_z() +parseFloat(ball0.get_acceler_z());
+  var posit_x= ball0.get_pos_x() + parseFloat(ball0.get_acceler_x());
+
+  var dx = ball1.get_pos_x() - posit_x;
+  var dz = ball1.get_pos_z() - posit_z;
   var dist = Math.sqrt(dx * dx + dz * dz);
 
   //collision handling code here
@@ -232,6 +259,7 @@ function checkCollision (ball0, ball1) {
 
         //collision reaction
         vxTotal = vel0.x - vel1.x;
+
     vel0.x = ((ball0.get_mass() - ball1.get_mass()) * vel0.x + 2 * ball1.get_mass() * vel1.x) /
              (ball0.get_mass() + ball1.get_mass());
     vel1.x = vxTotal + vel0.x;
@@ -257,10 +285,10 @@ function checkCollision (ball0, ball1) {
     ball1.change_accel_x_to(vel1F.x);
     ball1.change_accel_z_to(vel1F.z);
 
-    var position_z= ball1.get_pos_z() +parseFloat(ball1.get_acceler_z());
-    var position_x= ball1.get_pos_x() + parseFloat(ball1.get_acceler_x());
-    var posi_z= ball0.get_pos_z() +parseFloat(ball0.get_acceler_z());
-    var posi_x= ball0.get_pos_x() + parseFloat(ball0.get_acceler_x());
+    var position_z= ball1.get_pos_z() +parseFloat(ball1.get_acceler_z())/2;
+    var position_x= ball1.get_pos_x() + parseFloat(ball1.get_acceler_x())/2;
+    var posi_z= ball0.get_pos_z() +parseFloat(ball0.get_acceler_z())/2;
+    var posi_x= ball0.get_pos_x() + parseFloat(ball0.get_acceler_x())/2;
     ball0.put_x_z(posi_x,posi_z);   
     ball1.put_x_z(position_x,position_z);
     check_wall(ball0);
@@ -295,14 +323,20 @@ function diferent_pos(x,z,num){
 }
 
 function move_balls(){
-
     for(i=0;i<ball_num;i++){
+        //console.log(ball[i].get_acceler_x());
+        //console.log(ball[i].get_acceler_z())
         if(collision(ball[i].get_pos_x(), ball[i].get_pos_z(),i )==false){
-            var position_z= ball[i].get_pos_z() +parseFloat(ball[i].get_acceler_z());
-            var position_x= ball[i].get_pos_x() + parseFloat(ball[i].get_acceler_x());
+            var old_position_z = ball[i].get_pos_z()
+            var old_position_x = ball[i].get_pos_x()
+            var position_z= old_position_z +parseFloat(ball[i].get_acceler_z());
+            var position_x= old_position_x + parseFloat(ball[i].get_acceler_x());
+            ball[i].rotation.z += (position_z - old_position_z) / ball[i].get_radius()
+            ball[i].rotation.x += (position_x - old_position_x) / ball[i].get_radius()
+            //ball[i].rotateOnAxis ((0,0,1), ball[i].rotation.z) 
             ball[i].put_x_z(position_x,position_z);
             check_wall(ball[i]);
-            ball[i].change_position();
+            ball[i].change_position()
         }
 
     }
@@ -326,7 +360,7 @@ function create_balls(){
             num++;
         }
     }
-    console.log(ball);
+    //console.log(ball);
 }
 
 function createScene() {
@@ -336,12 +370,14 @@ function createScene() {
     new Floor(0,0,0);
     create_walls();
     create_balls();
+    
+    
 }
 
 function createCamera() {
     'use strict';
-    camera1 = new THREE.OrthographicCamera(wWidth / -2, wWidth / 2, wHeight / 2, wHeight / -2 , 150, 10000);
-    camera1.position.y = 400;
+    camera1 = new THREE.OrthographicCamera(wWidth / -4, wWidth / 4, wHeight / 4, wHeight / -4 , 150, 1000);
+    camera1.position.y = 200;
     camera2 = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight,150, 10000);
     camera2.position.z = -200;
     camera2.position.x = 300;
@@ -353,11 +389,56 @@ function createCamera() {
     camera4=new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera4.position.z=ball[0].get_pos_x();
     camera4.position.x=ball[0].get_pos_z();
-    camera4.position.y=80;
+    camera4.position.y=50;
    // camera.lookAt(scene.position);
 }
 
 function onResize() {
+    'use strict'
+
+    screen = resize_Aux()
+
+    if (window.innerWidth > 0 && window.innerHeight > 0) {
+
+            if (perspetiva==1)
+                camera.aspect = screen.w / screen.h
+
+            else if (ortegonal==1) {
+                camera.left = screen.w * -ZOOM
+                camera.right = screen.w * ZOOM
+                camera.top = screen.h * ZOOM
+                camera.bottom = screen.h * -ZOOM
+            }
+
+            camera.updateProjectionMatrix()
+    }
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+function resize_Aux() {
+    'use strict'
+    var width;
+    var height;
+    var scale = window.innerWidth / window.innerHeight
+
+    if(scale > ASPECT) { // higher width
+        // height = window.innerHeight
+        // width = height * ASPECT
+        width = scale * 50;
+        height = 50;
+    }
+    else { // higher height
+        // width = window.innerWidth
+        // height = width / ASPECT
+        width = ASPECT * 50;
+        height = width / scale;
+    }
+
+    return {w: width, h: height}
+}
+
+/*function onResize() {
     'use strict';
     var sceneSize = 300;
     wWidth = window.innerWidth;
@@ -372,7 +453,7 @@ function onResize() {
     camera.updateProjectionMatrix();
     renderer.setSize(wWidth, wHeight);
     camera.lookAt(scene.position);
-}
+}*/
 
 function render() {
     'use strict';
@@ -384,19 +465,35 @@ function onKeyDown(e) {
     switch (e.keyCode) {
     case 49: //1
         camera=camera1;
+        perspetiva=0;
+        ortegonal=1;
+        ball_camera=0;
         render();
         break;
     case 50://2
         camera=camera2;
+        perspetiva=1;
+        ortegonal=0;
+        ball_camera=0;
+        camera.lookAt(scene.position);
         render();
         break;
     case 51://3
         camera=camera3;
+        perspetiva=1;
+        ortegonal=0;
+        ball_camera=0;
+        camera.lookAt(scene.position);
         render();
         break;
     case 52:
         camera=camera4;
+        perspetiva=1;
+        ball_camera=1;
+        ortegonal=0;
         camera.lookAt(ball[0].position);
+        render();
+        break;
     case 69:  //E
     case 101: //e
         scene.traverse(function (node) {
@@ -413,6 +510,7 @@ function init() {
     renderer = new THREE.WebGLRenderer({
         antialias: true
     });
+    
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     createScene();
@@ -423,6 +521,7 @@ function init() {
     onResize();
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("resize", onResize);
+    setInterval(add_accel,1000*60);
 }
 
 function animate() {
